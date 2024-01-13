2ff33157c1d4ccc97f70d3a796c429037b928707
Daniil Utkin <d.utkin@tesonero.com>
2024-01-13 15:59:19 +0300

    fix deploy

--- src/pages/NoMatchPage.tsx
+++ src/pages/NoMatchPage.tsx
@@ -4,12 +4,8 @@ import { Link } from 'react-router-dom';
 export const NoMatchPage = () => {
   return (
     <div>
-      <div>
-        404 страница не найдена
-      </div>
-
       <Link to="/">
-        Назад к списку постов
+        Перейти к списку постов
       </Link>
     </div>
   );
